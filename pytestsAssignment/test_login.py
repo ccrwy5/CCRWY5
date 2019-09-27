348070ffd6acd52cc6af2c557dc0cd878b78141e
Chris Rehagen <ccrwy5@mail.missouri.edu>
2019-09-26 19:02:12 -0500

    updated

--- pytestsAssignment/test_login.py
+++ pytestsAssignment/test_login.py
@@ -14,6 +14,10 @@ def test_login_FAIL():
 	assert userName != '' and password != ''
 
 
+
+
+
+
 # Test 2
 
 def test_math():
@@ -26,3 +30,43 @@ def test_math_FAIL():
 
 	assert expectedResult == (9 * 3)
 
+
+
+
+
+# Test 3
+
+def test_find_student():
+	fname = login.getStudentFirstName()
+	lname = login.getStudentLastName()
+
+	assert fname == 'Michael' and lname == 'Jones'
+
+def test_find_student_FAIL():
+	fname = login.getStudentFirstNameFAIL()
+	lname = login.getStudentLastNameFAIL()
+	
+	assert fname != '' and lname != ''
+
+
+# Test 4
+
+def test_check_courseID():
+	courseID = login.getCourseID()
+	assert courseID == 'CS4230'
+
+def test_check_courseIDFAIL():
+	courseID = login.getCourseIDFAIL()
+	assert courseID == 'CS4230'
+
+
+# Test 5
+
+def test_check_instructor_info():
+	id = login.getInstructorID()
+
+	assert id == "9999"
+
+def test_check_instructor_info_FAIL():
+	id = login.getInstructorIDFAIL()
+	assert id != ""
